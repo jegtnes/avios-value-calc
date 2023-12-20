94a0ec46c2c313858cfb9c7f4247c05ae4a330ec
Alex Jegtnes <alex@jegtnes.com>
2023-12-20 18:09:22 +0100

    Add koa body processing

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import Koa from 'koa';
+const { koaBody } = require('koa-body');
 // @ts-ignore
 import nunjucks from 'koa-nunjucks-async';
 
 const app = new Koa();
+app.use(koaBody());
 
 app.use(nunjucks('src/views', {
     ext: '.njk'
 }));
 
-app.use(async (ctx: any) => {
+app.use(async (ctx: Koa.Context) => {
     await ctx.render('home', {
         title: "Avios value calculator",
-        message: 'Greetings, the universe!'
+        value: ctx.request.body.value,
     });
 });
 
