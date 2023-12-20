e32a7b7e6232c8d4b55dea6f1f85cebd65073cb8
Alex Jegtnes <alex@jegtnes.com>
2023-12-20 15:31:41 +0100

    Tidy

--- utils.js
+++ utils.js
@@ -1,29 +1,29 @@
-const VALUE_TRESHOLDS = {
-  TWO: {
+const VALUE_TRESHOLDS = [
+  {
     name: "2p/Avios",
     value: 200,
   },
-  ONE_POINT_FIVE: {
+  {
     name: "1.5p/Avios",
     value: 150,
   },
-  HFP: {
+  {
     name: "1p/Avios (HeadForPoints valuation)",
     value: 100,
   },
-  NECTAR: {
+  {
     name: "0.67p/Avios (Nectar points used for shopping)",
     value: 67,
   }, 
-  AMEX: {
+  {
     name: "0.5p/Avios (Amex statement credit)",
     value: 50,
   },
-  CUSTOM: {
+  {
     name: "Custom valuation",
     value: 0,
   }
-}
+];
 
 function calculateTotalValue(string, value) {
   return (
@@ -39,14 +39,11 @@ function parseText(text) {
   return text.split("\n").map((line) => line.trim());
 }
 
-//     11500 Avios + £ 135.00
 function getCashFromString(string) {
   const regex = new RegExp(/£\s?(\d*\.?\d*)/);
   const result = regex.exec(string);
   return Math.round(result[1]);
 }
-
-console.log(getCashFromString("11500 Avios + £ 135.00"));
 
 function getAviosFromString(string) {
   const regex = new RegExp(/^(\d*) Avios/);
@@ -54,15 +51,8 @@ function getAviosFromString(string) {
   return Math.round(result[1]);
 }
 
-console.log(
-  parseText(`    31000 Avios + £ 9.00
-26500 Avios + £ 25.00
-19500 Avios + £ 50.00
-14500 Avios + £ 95.00
-11500 Avios + £ 135.00`).forEach((l) => {
-    console.log(l, 'cash from string', getCashFromString(l));
-    console.log(l, 'avios from string', getAviosFromString(l));
-    console.log(l, 'TOTAL VALUE', calculateTotalValue(l, VALUE_TRESHOLDS.NECTAR.value));
-    console.log("-----");
-  })
-);
+module.exports = {
+  VALUE_TRESHOLDS,
+  calculateTotalValue,
+  parseText,
+}
